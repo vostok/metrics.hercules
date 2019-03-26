0d0231fa840889e52a6a0c0335e7dbe78ef63527
iloktionov <iloktionov@skbkontur.ru>
2019-03-26 19:45:49 +0500

    Implemented conversion between MetricEvents and HerculesEvents.

---
+++ Vostok.Metrics.Hercules.Tests/EventConversion_Tests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Vostok.Hercules.Client.Abstractions;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Metrics.Model;
+
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace Vostok.Metrics.Hercules.Tests
+{
+    [TestFixture]
+    internal class EventConversion_Tests
+    {
+        private HerculesMetricSenderSettings settings;
+        private HerculesMetricSender sender;
+        private HerculesEventBuilder builder;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            builder = new HerculesEventBuilder();
+
+            var sink = Substitute.For<IHerculesSink>(); 
+
+            sink
+                .WhenForAnyArgs(s => s.Put(null, null))
+                .Do(info => info.Arg<Action<IHerculesEventBuilder>>().Invoke(builder));
+
+            settings = new HerculesMetricSenderSettings(sink, Guid.NewGuid().ToString());
+
+            sender = new HerculesMetricSender(settings);
+        }
+
+        [Test]
+        public void Should_convert_simple_events()
+        {
+            var tags = MetricTags.Empty
+                .Append("k1", "v1")
+                .Append("k2", "v2");
+
+            var metricEvent = new MetricEvent(324.342d, tags, DateTimeOffset.UtcNow, null, null, null);
+
+            TestEventConversion(metricEvent);
+        }
+
+        [Test]
+        public void Should_convert_complex_events()
+        {
+            var tags = MetricTags.Empty
+                .Append("k1", "v1")
+                .Append("k2", "v2");
+
+            var metricEvent = new MetricEvent(324.342d, tags, DateTimeOffset.UtcNow, 
+                WellKnownUnits.Time.Milliseconds, WellKnownAggregationTypes.Counter, new Dictionary<string, string>
+                {
+                    ["Param1"] = "value1",
+                    ["Param2"] = "value2"
+                });
+
+            TestEventConversion(metricEvent);
+        }
+
+        private void TestEventConversion(MetricEvent metricEvent)
+        {
+            sender.Send(metricEvent);
+
+            var herculesEvent = builder.BuildEvent();
+
+            var convertedMetricEvent = HerculesMetricEventFactory.CreateFrom(herculesEvent);
+
+            convertedMetricEvent.Should().Be(metricEvent);
+        }
+    }
+}
---
+++ Vostok.Metrics.Hercules/HerculesMetricEventFactory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Metrics.Model;
+
+// ReSharper disable PossibleNullReferenceException
+
+namespace Vostok.Metrics.Hercules
+{
+    /// <summary>
+    /// Converts <see cref="HerculesEvent"/>s written by <see cref="HerculesMetricSender"/> back to <see cref="MetricEvent"/>s.
+    /// </summary>
+    [PublicAPI]
+    public static class HerculesMetricEventFactory
+    {
+        [NotNull]
+        public static MetricEvent CreateFrom([NotNull] HerculesEvent @event)
+        {
+            var timestamp = @event.Timestamp;
+            var tags = ConvertTags(@event.Tags[TagNames.Tags].AsVector.AsContainerList);
+            var value = @event.Tags[TagNames.Value].AsDouble;
+            var unit = @event.Tags[TagNames.Unit]?.AsString;
+            var aggregationType = @event.Tags[TagNames.AggregationType]?.AsString;
+            var aggregationParameters = ConvertAggregationParameters(@event.Tags[TagNames.AggregationParameters]?.AsContainer);
+
+            return new MetricEvent(value, tags, timestamp, unit, aggregationType, aggregationParameters);
+        }
+
+        private static MetricTags ConvertTags([NotNull] IReadOnlyList<HerculesTags> tags)
+        {
+            var result = MetricTags.Empty;
+
+            foreach (var tag in tags)
+                result = result.Append(tag[TagNames.Key].AsString, tag[TagNames.Value].AsString);
+
+            return result;
+        }
+
+        private static IReadOnlyDictionary<string, string> ConvertAggregationParameters([CanBeNull] HerculesTags tags)
+        {
+            if (tags == null || tags.Count == 0)
+                return null;
+
+            var result = new Dictionary<string, string>(tags.Count);
+
+            foreach (var tag in tags)
+                result[tag.Key] = tag.Value.AsString;
+            
+            return result;
+        }
+    }
+}
