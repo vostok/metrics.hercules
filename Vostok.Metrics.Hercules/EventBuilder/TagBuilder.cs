fb66311eb06c6034b6e14a3bc59af1b511074dee
kungurtsev <kungurtsev@skbkontur.ru>
2019-06-17 15:46:31 +0500

    new event builder

---
+++ Vostok.Metrics.Hercules/EventBuilder/TagBuilder.cs
@@ -0,0 +1,33 @@
+﻿using JetBrains.Annotations;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Metrics.Models;
+// ReSharper disable ParameterHidesMember
+
+namespace Vostok.Metrics.Hercules.EventBuilder
+{
+    internal class TagBuilder : DummyHerculesTagsBuilder, IHerculesTagsBuilder
+    {
+        private string key;
+        private string value;
+        
+        IHerculesTagsBuilder IHerculesTagsBuilder.AddValue(string key, string value)
+        {
+            switch (key)
+            {
+                case TagNames.Key:
+                    this.key = value;
+                    break;
+                case TagNames.Value:
+                    this.value = value;
+                    break;
+            }
+
+            return this;
+        }
+
+        public MetricTag Build()
+        {
+            return new MetricTag(key, value);
+        }
+    }
+}
